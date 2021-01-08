ac98ff95b551f78c62ab1bdf768fa8993e1f8a90
Xavier <xaviersercia@live.ca>
2021-01-08 17:59:29 -0500

    Finalisation de l'alpha.
    Création des controls du personnage, création du système de tilemap, création de la monté de la lave.

---
+++ Assets/Scripts/LavaHandler.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class LavaHandler : MonoBehaviour
+{
+    [SerializeField] Tile lavaTile = null;
+    Tilemap lavaGrid;
+
+    [SerializeField] float speedInterval = 2;
+    [SerializeField] int ascensionHeight = 1;
+
+    [Header("Bounds of Lava")]
+    [SerializeField] int maxYPosition = 6;
+    [SerializeField] int minXPosition = -2;
+    [SerializeField] int maxXPosition = 20;
+
+
+    [SerializeField] int currentHeight = 1;
+
+    [SerializeField] Transform lavaLight = null;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lavaGrid = GetComponent<Tilemap>();
+
+        //lavaGrid.BoxFill(new Vector3Int(0, 0, 0), lavaTile, minXPosition, 0, maxXPosition, 50);
+
+        StartCoroutine(LavaAscension());
+    }
+
+    IEnumerator LavaAscension()
+    {
+
+        //Debug.Log("hello !?");
+        while(currentHeight < maxYPosition)
+        {
+            yield return new WaitForSeconds(speedInterval);
+
+            lavaGrid.size = new Vector3Int(24, currentHeight + ascensionHeight, 0);
+            //lavaGrid.size = new Vector3Int(24, 10, 0);
+            lavaGrid.BoxFill(new Vector3Int(0, currentHeight, 0), lavaTile, minXPosition, currentHeight, maxXPosition, currentHeight + ascensionHeight - 1);
+            //lavaGrid.BoxFill(new Vector3Int(0,2,0), lavaTile, minXPosition, 2, maxXPosition, 4);
+            lavaLight.position = new Vector2(0, lavaLight.position.y + ascensionHeight);
+            currentHeight += ascensionHeight;
+
+        }
+
+
+    }
+
+    void StartInterval()
+    {
+        StartCoroutine(LavaAscension());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMovement : MonoBehaviour
+{
+	CharacterController2D controller;
+	Rigidbody2D rb;
+	Animator anim;
+
+	float HorizontalMove = 0f;
+	[SerializeField] float speed = 50f;
+	bool jump = false;
+
+	[SerializeField] Vector2 velocity;
+
+	Vector2 lastPosition;
+
+	private void Start()
+	{
+		controller = GetComponent<CharacterController2D>();
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+
+		lastPosition = transform.position;
+	}
+
+	private void Update()
+	{
+		velocity = rb.velocity;
+
+		HorizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+			//anim.SetBool("Running", false);
+			anim.SetBool("Jumping", true);
+		}
+
+
+		/*if(rb.velocity == new Vector2(0, 0))
+		{
+			anim.SetBool("Falling", false);
+			anim.SetBool("Jumping", false);
+			//anim.SetBool("Running", false);
+		}*/
+
+		if(rb.velocity.y < -0.5f)
+		{
+			anim.SetBool("Falling", true);
+			anim.SetBool("Jumping", false);
+			//anim.SetBool("Running", false);
+		}
+		else
+		{
+			anim.SetBool("Falling", false);
+
+		}
+
+		anim.SetFloat("Speed", Mathf.Abs(HorizontalMove));
+
+		/*if (dist > 0.05f || dist < -0.05f)
+		{
+			anim.SetBool("Falling", false);
+			anim.SetBool("Jumping", false);
+			anim.SetBool("Running", true);
+		}
+
+		if (rb.velocity.x != 0 && rb.velocity.y == 0 && !jump)
+		{
+
+		}
+
+		lastPosition = transform.position;
+		*/
+	}
+
+
+	private void FixedUpdate()
+	{
+		controller.Move(HorizontalMove * Time.fixedDeltaTime, false, jump);
+		jump = false;
+	}
+
+
+}
